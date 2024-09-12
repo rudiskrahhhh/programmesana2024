db1279305839b818b94c33d870e2fdfadc8b6a06
rudiskrahhhh <rudis.kruskops@gmail.com>
2024-09-12 16:04:05 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,7 +5,26 @@ class Cilveks :
         self.age=vecums
     def dzimene(self) :
         self.age +=1
-    def Bazars(self) :
+        self.bazars()
+    
+    def mainit_dzimumu(self, jaunais_dzimums):
+        if jaunais_dzimums == "":
+            if self.sex == "s":
+                self.sex = "v"
+            else:
+                self.sex = "s"
+        else:
+            self.sex = jaunais_dzimums
+        self.bazars()
+            
+    def mainit_vardu(self, jaunais_vards):
+        self.name = jaunais_vards
+        self.bazars()
+
+
+
+
+    def bazars(self) :
         if self.sex == "V":
             dzimums = "vīrietis"
         elif self.sex == "S":
@@ -14,8 +33,8 @@ class Cilveks :
             dzimums = self.sex
         print("Sveiki, mani sauc {}, es esmu {} gadus vecs. Es esmu {}".format(self.name, self.age, dzimums))
 	
-persona=Cilveks("Rudis","kjhakjhkjh",18)
+persona=Cilveks("Rudis","V",18)
 
 persona.dzimene()
 
-persona.Bazars() 
+persona.bazars() 
