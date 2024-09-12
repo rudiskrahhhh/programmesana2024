f6ee30033d93a4634872232b5ce43f47ab49ea89
rudiskrahhhh <rudis.kruskops@gmail.com>
2024-09-12 16:39:43 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,11 +1,11 @@
 class Cilveks : 
-    def __init__(self,vards,dzimums,vecums) :
+    def __init__(self, vards, dzimums, vecums = 0) :
         self.name=vards
         self.sex=dzimums
         self.age=vecums
     def dzimene(self) :
         self.age +=1
-        self.bazars()
+
     
     def mainit_dzimumu(self, jaunais_dzimums):
         if jaunais_dzimums == "":
@@ -21,9 +21,6 @@ class Cilveks :
         self.name = jaunais_vards
         self.bazars()
 
-
-
-
     def bazars(self) :
         if self.sex == "V":
             dzimums = "vīrietis"
@@ -32,9 +29,25 @@ class Cilveks :
         else:
             dzimums = self.sex
         print("Sveiki, mani sauc {}, es esmu {} gadus vecs. Es esmu {}".format(self.name, self.age, dzimums))
+
+
+
+class Sieviete(Cilveks):
+    def __init__(self, vards, hair_color , vecums = 0) :
+        super().__init__(vards, "S", vecums)
+        self.matu_krasa = hair_color
+        self.bazars()
+
+    def bazars(self):
+        super().bazars()
+        print("Mana matu krāsa ir", self.matu_krasa)
+
+
+
+    
 	
-persona=Cilveks("Rudis","V",18)
+persona=Cilveks("Rudis","V", 18)
+persona1=Sieviete("Katrina", "riza", 19)
 
 persona.dzimene()
-
 persona.bazars() 
